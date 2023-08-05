fc067076a3b558e99b9a2f76e5567a228081b477
Artem Kopytko <artem.kop19@gmail.com>
2023-08-05 13:52:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _242._Valid_Anagram
+{
+    internal class Program
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if(s.Length != t.Length) return false;
+
+            var dict = new Dictionary<char, int>(s.Length);
+            foreach( char ch in s )
+            {
+                if(!dict.ContainsKey(ch))
+                {
+                    dict.Add(ch, 1);
+                }
+                else
+                {
+                    dict[ch]++;
+                }
+            }
+
+            foreach(char ch in t)
+            {
+                if (!dict.ContainsKey(ch)) return false;
+                dict[ch]--;
+            }
+            return !dict.Values.Any(n => n != 0);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
